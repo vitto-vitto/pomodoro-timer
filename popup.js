ea085d347831f5fd0f65fb2acbb414182b7775c9
vitto-vitto <vic.andre1410@gmail.com>
2021-11-28 21:48:43 -0300

    feature: Timer setup on background.js and testing use on popup.js

--- popup.js
+++ popup.js
@@ -1,15 +1,19 @@
 const timeElement = document.getElementById("time");
 const nameElement = document.getElementById("name");
+const timerElement = document.getElementById("timer");
+ 
 
- const currentTime = new Date().toLocaleTimeString();
+function updateTimeElements(){
+    chrome.storage.local.get(["timer"], (res) => {
+        const time = res.timer ?? 0;
+        timerElement.textContent = `Timer at ${time} seconds`
+    })
+    const currentTime = new Date().toLocaleTimeString()
+     timeElement.textContent = `Time is current: ${currentTime}`;
+}
 
- timeElement.textContent = `Time is current: ${currentTime}`;
-
- chrome.action.setBadgeText({
-     text: "",
- }, () => {
-     console.log("Finished set Badge ID")
- });
+updateTimeElements()
+setInterval(updateTimeElements, 1000)
 
  chrome.storage.sync.get(["name"], (res) => {
     const name = res.name ?? "???"
