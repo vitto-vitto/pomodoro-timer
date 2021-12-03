73273a3a75465b156803c7dfeae92d4be61d9de2
vitto-vitto <vic.andre1410@gmail.com>
2021-12-03 00:38:02 -0300

    feature: Start and reset timer button linked to chrome local storage

---
+++ background.js
@@ -0,0 +1,33 @@
+chrome.alarms.create("pomodoroTimer", {
+    periodInMinutes: 1/60,
+})
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+    if(alarm.name === "pomodoroTimer"){
+        chrome.storage.local.get(["timer", "isRunning"], (res) => {
+            if (res.isRunning) {
+                let timer = res.timer + 1
+                let isRunning = true
+                if(timer === 60*25){
+                    this.ServiceWorkerRegistration.showNotification("Pomodoro Timer", {
+                        body: "25 minutes has passed!",
+                        icon: "icon.png"
+                    })
+                    timer = 0
+                    isRunning = false
+                }
+                chrome.storage.local.set({
+                    timer,
+                    isRunning
+                })
+            }
+        })
+    }
+})
+
+chrome.storage.local.get(["timer", "isRunning"], (res) => {
+    chrome.storage.local.set({
+        timer: "timer" in res ? res.timer : 0,
+        isRunning: "isRunning" in res ? res.isRunning : false,
+    })
+})
